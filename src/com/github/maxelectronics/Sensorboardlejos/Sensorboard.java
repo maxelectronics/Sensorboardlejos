540312685755d8f4a9ef1d98d8e29f369f569ed3
Max Heidbrink <max.electronics@me.com>
2016-10-27 11:44:37 +0200

    Added thread support

--- src/com/github/maxelectronics/Sensorboardlejos/Sensorboard.java
+++ src/com/github/maxelectronics/Sensorboardlejos/Sensorboard.java
@@ -1,10 +1,11 @@
-package com.github.maxelectronics.Sensorboardlejos;
-
 import lejos.nxt.I2CPort;
+import lejos.util.Delay;
 
 /**
  * Java-Class to access the sensorboard from an NXT with leJOS.
- * Created by Max (max.electronics-at-me.com),
+ * Created by Max (max.electronics-at-me.com).
+ * 
+ * This class is intended to be used in your projects as-is. DON'T change anything!
  * 
  * License: GPL 2.0
  *
@@ -13,10 +14,10 @@ import lejos.nxt.I2CPort;
  *
  * This code is self-documenting (or I'm just too lazy to write comments)
  */
-public class Sensorboard {
+public class Sensorboard extends Thread {
 	/**
 	 * A custom Color object used for the color sensors.
-	 * Contains four public 16-bit values (0...65536) representing each color (red, green, blue and clear).
+	 * Contains four public values (0...1024 at default timing) representing each color (red, green, blue and clear).
 	 */
 	public class Color {
 		public int red;
@@ -26,10 +27,10 @@ public class Sensorboard {
 		
 		/**
 		 * Constructor to create a Color object.
-		 * @param red New red 16bit value (0...65535)
-		 * @param green New green 16bit value (0...65535)
-		 * @param blue New blue 16bit value (0...65535)
-		 * @param clear New clear 16bit value (0...65535)
+		 * @param red New red value (0...1024 at default timing)
+		 * @param green New green value (0...1024 at default timing)
+		 * @param blue New blue value (0...1024 at default timing)
+		 * @param clear New clear value (0...1024 at default timing)
 		 */
 		public Color(int red, int green, int blue, int clear) {
 			this.red = red;
@@ -40,6 +41,7 @@ public class Sensorboard {
 	}
 	
     private I2CPort port;
+    private Color allSensors[];
     
     /**
      * Initializes the SensorPort for use with the Sensorboard.
@@ -47,7 +49,44 @@ public class Sensorboard {
      */
     public Sensorboard(I2CPort port) {
         this.port = port;
-        this.port.i2cEnable(0);
+        this.port.i2cEnable(10);
+        
+        this.start();
+    }
+    
+    /**
+     * Initializes the SensorPort for use with the Sensorboard.
+     * @param port The SensorPort to initialize
+     * @param brightness Brightness of the LEDs
+     * @param gain Sensor gain, accepted values are 1, 4, 16, 60
+     * @param cycles Sensor Timing, less cycles means higher sample rate (default: 1)
+     */
+    public Sensorboard(I2CPort port, int brightness, int gain, int cycles) {
+    	this(port);
+    	setBrightness(brightness, brightness);
+    	setSensorGain(gain);
+    	setTiming(cycles);
+    }
+    
+    /**
+     * Run method of this thread. Gets all the sensor data off the I2C bus periodically.
+     */
+    public void run() {
+    	while(true) {
+	    	Color sensors[] = new Color[5];
+	    	for(int i = 0; i < 5; i++) {
+	    		byte write[] = {(byte)(0x55 + i)};
+	    		byte read[] = new byte[8];
+	    		port.i2cTransaction(48, write, 0, 1, read, 0, 8);
+	    		sensors[i] = new Color(
+		    			(int)(read[2] & 0xFF) + (int)(read[3] & 0xFF)*256,
+		    			(int)(read[4] & 0xFF) + (int)(read[5] & 0xFF)*256,
+		    			(int)(read[6] & 0xFF) + (int)(read[7] & 0xFF)*256,
+		    			(int)(read[0] & 0xFF) + (int)(read[1] & 0xFF)*256);
+	    	}
+	    	allSensors = sensors;
+	    	Delay.msDelay(1);
+    	}
     }
     
     /**
@@ -95,78 +134,79 @@ public class Sensorboard {
     }
     
     /**
+     * Sets the timing for each sample.
+     * @param cycles Less cycles means higher sample rate, but lower output reading (default: 1)
+     */
+    public void setTiming(int cycles) {
+    	if(cycles > 256) cycles = 1;
+    	else if(cycles < 1) cycles = 255;
+    	else cycles = 256-cycles;
+    	byte write[] = {0x51, (byte)(cycles & 0xFF)};
+    	port.i2cTransaction(48, write, 0, 2, null, 0, 0);
+    }
+    
+    /**
      * Reads all colors from all sensors.
      * @return An array containing five color objects, one for each sensor
      */
     public Color[] getAllSensorColors() {
-    	Color out[] = new Color[5];
-    	for(int i = 0; i < 5; i++)
-    		out[i] = getSensorColor(i);
-    	return out;
+    	return allSensors;
     }
     
     /**
      * Reads the red values from all Sensors.
-     * @return An array containing five 16bit values (0...65535), one for each sensor
+     * @return An array containing five values (0...1024 at default timing), one for each sensor
      */
     public int[] getRed() {
-    	byte write[] = {0x5B};
-    	byte read[] = new byte[10];
-    	port.i2cTransaction(48, write, 0, 1, read, 0, 10);
     	return new int[] {
-    			(int)(read[0] & 0xFF) + (int)(read[5] & 0xFF)*256,
-    			(int)(read[1] & 0xFF) + (int)(read[6] & 0xFF)*256,
-    			(int)(read[2] & 0xFF) + (int)(read[7] & 0xFF)*256,
-    			(int)(read[3] & 0xFF) + (int)(read[8] & 0xFF)*256,
-    			(int)(read[4] & 0xFF) + (int)(read[9] & 0xFF)*256};
+    			allSensors[0].red,
+    			allSensors[1].red,
+    			allSensors[2].red,
+    			allSensors[3].red,
+    			allSensors[4].red
+    	};
     }
 
     /**
      * Reads the green values from all Sensors.
-     * @return An array containing five 16bit values (0...65535), one for each sensor
+     * @return An array containing five values (0...1024 at default timing), one for each sensor
      */
     public int[] getGreen() {
-    	byte write[] = {0x5C};
-    	byte read[] = new byte[10];
-    	port.i2cTransaction(48, write, 0, 1, read, 0, 10);
     	return new int[] {
-    			(int)(read[0] & 0xFF) + (int)(read[5] & 0xFF)*256,
-    			(int)(read[1] & 0xFF) + (int)(read[6] & 0xFF)*256,
-    			(int)(read[2] & 0xFF) + (int)(read[7] & 0xFF)*256,
-    			(int)(read[3] & 0xFF) + (int)(read[8] & 0xFF)*256,
-    			(int)(read[4] & 0xFF) + (int)(read[9] & 0xFF)*256};
+    			allSensors[0].green,
+    			allSensors[1].green,
+    			allSensors[2].green,
+    			allSensors[3].green,
+    			allSensors[4].green
+    	};
     }
 
     /**
      * Reads the blue values from all Sensors.
-     * @return An array containing five 16bit values (0...65535), one for each sensor
+     * @return An array containing five values (0...1024 at default timing), one for each sensor
      */
     public int[] getBlue() {
-    	byte write[] = {0x5D};
-    	byte read[] = new byte[10];
-    	port.i2cTransaction(48, write, 0, 1, read, 0, 10);
     	return new int[] {
-    			(int)(read[0] & 0xFF) + (int)(read[5] & 0xFF)*256,
-    			(int)(read[1] & 0xFF) + (int)(read[6] & 0xFF)*256,
-    			(int)(read[2] & 0xFF) + (int)(read[7] & 0xFF)*256,
-    			(int)(read[3] & 0xFF) + (int)(read[8] & 0xFF)*256,
-    			(int)(read[4] & 0xFF) + (int)(read[9] & 0xFF)*256};
+    			allSensors[0].blue,
+    			allSensors[1].blue,
+    			allSensors[2].blue,
+    			allSensors[3].blue,
+    			allSensors[4].blue
+    	};
     }
 
     /**
      * Reads the clear values from all Sensors.
-     * @return An array containing five 16bit values (0...65535), one for each sensor
+     * @return An array containing five values (0...1024 at default timing), one for each sensor
      */
     public int[] getClear() {
-    	byte write[] = {0x5A};
-    	byte read[] = new byte[10];
-    	port.i2cTransaction(48, write, 0, 1, read, 0, 10);
     	return new int[] {
-    			(int)(read[0] & 0xFF) + (int)(read[5] & 0xFF)*256,
-    			(int)(read[1] & 0xFF) + (int)(read[6] & 0xFF)*256,
-    			(int)(read[2] & 0xFF) + (int)(read[7] & 0xFF)*256,
-    			(int)(read[3] & 0xFF) + (int)(read[8] & 0xFF)*256,
-    			(int)(read[4] & 0xFF) + (int)(read[9] & 0xFF)*256};
+    			allSensors[0].clear,
+    			allSensors[1].clear,
+    			allSensors[2].clear,
+    			allSensors[3].clear,
+    			allSensors[4].clear
+    	};
     }
 
     /**
@@ -175,15 +215,7 @@ public class Sensorboard {
      * @return A new Color object containing the four colors
      */
     public Color getSensorColor(int sensor) {
-    	if(sensor < 0 || sensor > 4) return null;
-    	byte write[] = {(byte)(0x55 + sensor)};
-    	byte read[] = new byte[8];
-    	port.i2cTransaction(48, write, 0, 1, read, 0, 8);
-    	return new Color(
-    			(int)(read[2] & 0xFF) + (int)(read[3] & 0xFF)*256,
-    			(int)(read[4] & 0xFF) + (int)(read[5] & 0xFF)*256,
-    			(int)(read[6] & 0xFF) + (int)(read[7] & 0xFF)*256,
-    			(int)(read[0] & 0xFF) + (int)(read[1] & 0xFF)*256);
+    	return allSensors[sensor];
     }
     
     /**
